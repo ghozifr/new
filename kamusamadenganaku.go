d2afad76c9df4e8ebce158e8f0d290f14792e766
ghozifr <ghozifikri@gmail.com>
2018-09-11 20:26:04 +0700

    learned

--- kamusamadenganaku.go
+++ kamusamadenganaku.go
@@ -1,12 +1,16 @@
 package main 
 
 import "fmt"
-
+var o = "AREA MENGHITUNG"
 func main() {
-	x:=100000000
-	y:=1001000
-	z:=("banyak ya")
+	x:=50
+	y:=50
+	z:=("50 ditambah 50 jadi sangling melengkapi")
 	g:= x + y 
+	fmt.Println(o)
 	fmt.Println(g)
 	fmt.Println(z)
+	//jenis apa itu o?
+	fmt.Printf("%T\n",o)
+	
 }
